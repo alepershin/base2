13809ec8800b17f78be98b27705890310ee3d761
Alex Pershin <alepershin@mail.ru>
2018-12-09 00:51:31 +0400

    Сделал проверку переполнения массива в методе save

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -12,8 +12,8 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        for(int i=0;i<size;i++){
-            storage[i]=null;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
         size = 0;
     }
@@ -36,8 +36,12 @@ public class ArrayStorage {
             i++;
         }
         if (i == size) {
-            storage[size] = r;
-            size++;
+            if (i < 9_999) {
+                storage[size] = r;
+                size++;
+            } else {
+                System.out.println("ERROR: Переполнение!");
+            }
         } else {
             System.out.println("ERROR: Резюме уже существует");
         }
