f6efe5a122f8a71db4fc6f7b81cd57d584974d2f
Alex Pershin <alepershin@mail.ru>
2018-12-22 22:20:19 +0400

    HW3 Got rid of code duplication

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -5,21 +5,9 @@ import com.urise.webapp.model.Resume;
 public class ArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) > -1) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    @Override
-    protected int getIndex(String uuid){
-        for (int i = 0; i < size; i++){
-            if (uuid.equals(storage[i].getUuid())){
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
@@ -27,15 +15,12 @@ public class ArrayStorage extends AbstractArrayStorage {
     }
 
     @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
+    protected void addResume(int index, Resume r) {
+        storage[size] = r;
     }
 
+    @Override
+    protected void delResume(int index) {
+        storage[index] = storage[size - 1];
+    }
 }
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -7,38 +7,23 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void save(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index > -1) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            int i = -index - 1;
-            if (i <= size - 1) {
-                if (size - i >= 0) System.arraycopy(storage, i, storage, i + 1, size - i);
-            }
-            storage[i] = r;
-            size++;
-        }
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            if (size - 1 - index >= 0) System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+
+    @Override
+    protected void addResume(int index, Resume r){
+        int i = -index - 1;
+        if (i <= size - 1) {
+            if (size - i >= 0) System.arraycopy(storage, i, storage, i + 1, size - i);
+        }
+        storage[i] = r;
+    }
+
+    @Override
+    protected void delResume(int index){
+        if (size - 1 - index >= 0) System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
     }
 }
