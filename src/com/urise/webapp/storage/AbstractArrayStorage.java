1c575bbd742926ee187f42bbe56cf321b56eeba6
Alex Pershin <alepershin@mail.ru>
2018-12-22 11:18:58 +0400

    HW3 Transferred clear() method to abstract class

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,37 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10_000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + "not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    protected abstract int getIndex(String uuid);
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+
+}
