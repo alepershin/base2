d1ad3b3dbecd9003bd469d929ecd3b95ef58917b
Alex Pershin <alepershin@mail.ru>
2019-03-10 08:05:16 +0400

    HW8: Created a class PeriodOrganization so that for each organization it was possible to establish several periods of work (training)

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -55,7 +55,7 @@ public class ResumeTestData {
                 "Родной русский, английский \"upper intermediate\""));
         resume.sections.put(QUALIFICATIONS, listSection);
 
-        ArrayList<Organization> organizations = new ArrayList<>();
+        ArrayList<Organization> itemExperience = new ArrayList<>();
 
         Link homePage = new Link("Java Online Projects", "http://javaops.ru/");
         LocalDate startDate = LocalDate.of(2013, OCTOBER, 1);
@@ -63,89 +63,89 @@ public class ResumeTestData {
         LocalDate endDate = LocalDate.of(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
         PeriodOrganization periodOrganization = new PeriodOrganization(startDate, endDate, "Автор проекта", "Создание, организация и проведение Java онлайн проектов и стажировок");
         Organization organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Wrike", "https://www.wrike.com/");
         startDate = LocalDate.of(2014, OCTOBER, 1);
         endDate = LocalDate.of(2016, JANUARY, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("RIT Center", "");
         startDate = LocalDate.of(2012, APRIL, 1);
         endDate = LocalDate.of(2014, OCTOBER, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Java архитектор", "Организация процесса разработки системы ERP для разных окружений: релизная политика, версионирование, ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения (почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Luxoft (Deutsche Bank)", "https://www.luxoft.com/");
         startDate = LocalDate.of(2010, DECEMBER, 1);
         endDate = LocalDate.of(2012, APRIL, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Ведущий программист", "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Yota", "https://www.yota.ru/");
         startDate = LocalDate.of(2008, JUNE, 1);
         endDate = LocalDate.of(2010, DECEMBER, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Ведущий специалист", "Дизайн и имплементация Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Enkata", "https://www.pega.com/enkata/");
         startDate = LocalDate.of(2007, MARCH, 1);
         endDate = LocalDate.of(2008, JUNE, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Разработчик ПО", "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining)");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Siemens AG", "https://new.siemens.com/ru/ru.html");
         startDate = LocalDate.of(2005, JANUARY, 1);
         endDate = LocalDate.of(2007, MARCH, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Разработчик ПО", "Разработка информационной модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе Siemens @vantage (Java, Unix)");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
         homePage = new Link("Alcatel", "http://www.alcatel.ru/");
         startDate = LocalDate.of(1997, SEPTEMBER, 1);
         endDate = LocalDate.of(2005, JANUARY, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM)");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemExperience.add(organization);
 
-        OrganizationSection organizationSection = new OrganizationSection(organizations);
+        OrganizationSection organizationSection = new OrganizationSection(itemExperience);
         resume.sections.put(EXPERIENCE, organizationSection);
 
-        organizations.clear();
+        ArrayList<Organization> itemEducation = new ArrayList<>();
 
         homePage = new Link("Coursera", "https://www.coursera.org/learn/progfun1");
         startDate = LocalDate.of(2013, MARCH, 1);
         endDate = LocalDate.of(2013, MAY, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "\"Functional Programming Principles in Scala\" by Martin Odersky", "");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
         homePage = new Link("Luxoft", "https://www.luxoft-training.ru/kurs/obektno-orientirovannyy__analiz_is_kontseptualnoe_modelirovanie_na_uml_dlya_sistemnyh_analitikov_.html");
         startDate = LocalDate.of(2011, MARCH, 1);
         endDate = LocalDate.of(2011, APRIL, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Курс \"Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.\"", "");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
         homePage = new Link("Siemens AG", "https://new.siemens.com/ru/ru.html");
         startDate = LocalDate.of(2005, JANUARY, 1);
         endDate = LocalDate.of(2005, APRIL, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "3 месяца обучения мобильным IN сетям (Берлин)", "");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
         homePage = new Link("Alcatel", "http://www.alcatel.ru/");
         startDate = LocalDate.of(1997, SEPTEMBER, 1);
         endDate = LocalDate.of(1998, MARCH, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "6 месяцев обучения цифровым телефонным сетям (Москва)", "");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
         homePage = new Link("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики", "http://www.ifmo.ru/ru/");
         startDate = LocalDate.of(1993, SEPTEMBER, 1);
@@ -155,16 +155,16 @@ public class ResumeTestData {
         endDate = LocalDate.of(1993, JULY, 1);
         PeriodOrganization period2 = new PeriodOrganization(startDate, endDate, "Инженер (программист Fortran, C)", "");
         organization = new Organization(homePage, Arrays.asList(period1, period2));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
         homePage = new Link("Заочная физико-техническая школа при МФТИ", "http://www.school.mipt.ru/");
         startDate = LocalDate.of(1984, SEPTEMBER, 1);
         endDate = LocalDate.of(1987, JUNE, 1);
         periodOrganization = new PeriodOrganization(startDate, endDate, "Закончил с отличием", "");
         organization = new Organization(homePage, Arrays.asList(periodOrganization));
-        organizations.add(organization);
+        itemEducation.add(organization);
 
-        organizationSection = new OrganizationSection(organizations);
+        organizationSection = new OrganizationSection(itemEducation);
         resume.sections.put(EDUCATION, organizationSection);
 
         System.out.println(resume.toString());
